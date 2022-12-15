6a57da3bdf1b69f1fcb3ead6c64f86ca8afc4906
NicolaPlacentino <nyko0921@gmail.com>
2022-12-15 16:48:56 +0100

    added ex 2 generateNumber function

--- jssnack2/script.js
+++ jssnack2/script.js
@@ -1,6 +1,19 @@
 // Pari e Dispari
-// L’utente sceglie pari o dispari e inserisce un numero da 1 a 5 tramiite un form.
+// L’utente sceglie pari o dispari e inserisce un numero da 1 a 5 tramite un form.
 // Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
 // Sommiamo i due numeri
 // Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 // Dichiariamo chi ha vinto.
+
+const userNumber = document.getElementById('user-number')
+const radio = document.getElementsByName('even-odd')
+const button = document.getElementById('button')
+const form = document.getElementById('form')
+
+function randomNumber() {
+    const random = Math.random()
+    const randomNumber = Math.floor(random * 5 + 1)
+
+    return randomNumber
+}
+
