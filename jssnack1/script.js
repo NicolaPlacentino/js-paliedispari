6b79e5d2dc0366e2814be94b1561fb99cbf00f76
NicolaPlacentino <nyko0921@gmail.com>
2022-12-15 16:38:56 +0100

    completed exercise 1

--- jssnack1/script.js
+++ jssnack1/script.js
@@ -2,9 +2,10 @@
 // Chiedere all’utente di inserire una parola tramite un form
 // Creare una funzione per capire se la parola inserita è palindroma
 
-const userWord = document.getElementById('user-word')
+const userWordArea = document.getElementById('user-word')
 const button = document.getElementById('button')
 const form = document.getElementById('form')
+const finalMessageArea = document.getElementById('final-message')
 
 function reverseWord(word) {
 
@@ -16,3 +17,21 @@ function reverseWord(word) {
 
     return result
 }
+
+form.addEventListener("submit", function(event){
+    event.preventDefault()
+
+    const userWord = userWordArea.value.toLowerCase()
+
+    const reversedWord = reverseWord(userWord)
+
+    let finalmessage = ''
+
+    if (reversedWord === userWord){
+        finalmessage = `"${userWord}" è palindroma`
+    } else {
+        finalmessage = `"${userWord}" non è palindroma, al contrario sarebbe "${reversedWord}"`
+    }
+
+    finalMessageArea.innerText = finalmessage
+})
