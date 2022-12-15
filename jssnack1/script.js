ca40dd27dc747b6dba1402be2e58636858ec554a
NicolaPlacentino <nyko0921@gmail.com>
2022-12-15 16:25:29 +0100

    added reverse word function

--- jssnack1/script.js
+++ jssnack1/script.js
@@ -1,3 +1,18 @@
 // Palidroma
-// Chiedere all’utente di inserire una parola tramiite un form
+// Chiedere all’utente di inserire una parola tramite un form
 // Creare una funzione per capire se la parola inserita è palindroma
+
+const userWord = document.getElementById('user-word')
+const button = document.getElementById('button')
+const form = document.getElementById('form')
+
+function reverseWord(word) {
+
+    let result = ''
+
+    for (let i = word.length - 1; i >= 0; i--) {
+        result += word[i]
+    }
+
+    return result
+}
